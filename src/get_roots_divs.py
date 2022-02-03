c87e0856172e5c269edea1419193c505909c4182
Adami Turabi <adamiturabi@protonmail.com>
2022-02-03 21:37:47 +0500

    added line break in text; sorting root indexes for easy diffing

--- src/get_roots_divs.py
+++ src/get_roots_divs.py
@@ -1,6 +1,6 @@
 def get_roots_divs(roots_set):
   out_str = ''
-  for root in roots_set:
+  for root in sorted(roots_set):
     out_str += '<div style="visibility: hidden;" id="index_'+root+'"></div>\n'
   return out_str
 
