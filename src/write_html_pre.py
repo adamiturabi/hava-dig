f5e550fb46f3d18787fd555fc4d8e2465f3d84c7
Adami Turabi <adamiturabi@protonmail.com>
2022-02-03 11:55:18 +0500

    updated jquery link

--- src/write_html_pre.py
+++ src/write_html_pre.py
@@ -37,7 +37,7 @@ def write_html_pre(fout):
           }
         </style>
         <title>Hava: Arabic-English Dictionary</title>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="searchroot.js"></script>
     </head>
     <body>
