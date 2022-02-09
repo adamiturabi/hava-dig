b369b946720a9f23115f2533e70d77ca7a4130f5
Adami Turabi <adamiturabi@protonmail.com>
2022-02-09 11:02:37 +0500

    Added more tests

--- src/hamzater.py
+++ src/hamzater.py
@@ -10,12 +10,12 @@ class ConsonantInfo:
   vowel = ""
   hmz = ""
   post_cons_str = ""
-  suf = False
+  suff = False
   def __str__(self):
-    return "cons="+self.cons+", doubled="+str(self.doubled)+", vowel_mark="+self.vowel_mark+", vowel="+self.vowel+", suf="+str(self.suf)
+    return "cons="+self.cons+", doubled="+str(self.doubled)+", vowel_mark="+self.vowel_mark+", vowel="+self.vowel+", suff="+str(self.suff)
 
-def build_cons_list(basestr, suf):
-  instr = basestr + suf
+def build_cons_list(basestr, suff):
+  instr = basestr + suff
   cons_list = []
   index = 0
   while index < len(instr):
@@ -25,11 +25,11 @@ def build_cons_list(basestr, suf):
     cons_info = ConsonantInfo()
     cons_info.cons = instr[index]
     if index >= len(basestr):
-      cons_info.suf = True
+      cons_info.suff = True
     index += 1
     if index < len(instr) and instr[index] == o:
       if index >= len(basestr):
-        cons_info.suf = True
+        cons_info.suff = True
       cons_info.vowel_mark = o
       cons_info.vowel = o
       cons_info.post_cons_str += o
@@ -38,38 +38,38 @@ def build_cons_list(basestr, suf):
       continue
     if index < len(instr) and instr[index] == ss:
       if index >= len(basestr):
-        cons_info.suf = True
+        cons_info.suff = True
       cons_info.doubled = True
       cons_info.post_cons_str += ss
       index += 1
     if index < len(instr) and instr[index] == a:
       if index >= len(basestr):
-        cons_info.suf = True
+        cons_info.suff = True
       cons_info.vowel_mark = a
       cons_info.vowel = a
       cons_info.post_cons_str += a
       index += 1
       if index < len(instr) and instr[index] == A:
         if index >= len(basestr):
-          cons_info.suf = True
+          cons_info.suff = True
         cons_info.vowel += A
         cons_info.post_cons_str += A
         index += 1
       elif index < (len(instr)-1) and instr[index:(index+1)] == Y:
         if index >= len(basestr):
-          cons_info.suf = True
+          cons_info.suff = True
         cons_info.vowel += A
         cons_info.post_cons_str += Y
         index+=2
       elif index < (len(instr)-1) and instr[index:(index+2)] == y+o:
         if index >= len(basestr):
-          cons_info.suf = True
+          cons_info.suff = True
         cons_info.vowel += y+o
         cons_info.post_cons_str += y+o
         index += 2
       elif index < (len(instr)-1) and instr[index:(index+2)] == w+o:
         if index >= len(basestr):
-          cons_info.suf = True
+          cons_info.suff = True
         cons_info.vowel += w+o
         cons_info.post_cons_str += w+o
         index += 2
@@ -77,14 +77,14 @@ def build_cons_list(basestr, suf):
       continue
     if index < len(instr) and instr[index] == i:
       if index >= len(basestr):
-        cons_info.suf = True
+        cons_info.suff = True
       cons_info.vowel_mark = i
       cons_info.vowel = i
       cons_info.post_cons_str += i
       index += 1
       if (index == (len(instr)-1) and instr[index] == y) or (index < (len(instr)-1) and instr[index] == y and instr[index+1] in letters):
         if index >= len(basestr):
-          cons_info.suf = True
+          cons_info.suff = True
         cons_info.vowel += y
         cons_info.post_cons_str += y
         index += 1
@@ -92,14 +92,14 @@ def build_cons_list(basestr, suf):
       continue
     if index < len(instr) and instr[index] == u:
       if index >= len(basestr):
-        cons_info.suf = True
+        cons_info.suff = True
       cons_info.vowel_mark = u
       cons_info.vowel = u
       cons_info.post_cons_str += u
       index += 1
       if (index == (len(instr)-1) and instr[index] == w) or (index < (len(instr)-1) and instr[index] == w and instr[index+1] in letters):
         if index >= len(basestr):
-          cons_info.suf = True
+          cons_info.suff = True
         cons_info.vowel += w
         cons_info.post_cons_str += w
         index += 1
@@ -107,7 +107,7 @@ def build_cons_list(basestr, suf):
       continue
     if index < len(instr) and instr[index] in { N, F, K}:
       if index >= len(basestr):
-        cons_info.suf = True
+        cons_info.suff = True
       cons_info.vowel_mark = instr[index]
       cons_info.vowel = instr[index]
       cons_info.post_cons_str += instr[index]
@@ -135,11 +135,11 @@ def build_str(cons_list):
   retval = retval.replace(A+hmz_ab+a+A+hmz_ab+o, A_md)
   return retval
 
-def hamzate(instr, suf):
-  cons_list = build_cons_list(instr, suf)
-  cons_list_base = build_cons_list(instr, "")
+def hamzate(base_str, suff_str="", pref_str=""):
+  cons_list = build_cons_list(base_str, suff_str)
+  cons_list_base = build_cons_list(base_str, "")
   # For debugging:
-  #if (instr == "buTo'a"):
+  #if (base_str == "buTo'a"):
   #  print_cons_list(cons_list)
   for index in range(0, len(cons_list)):
     x = cons_list[index]
@@ -178,7 +178,7 @@ def hamzate(instr, suf):
             if x.vowel_mark == i:
               x.hmz = y+hmz_ab
             else:
-              if x.vowel == a+A and x.post_cons_str[-1] == A and not x.doubled and not x.suf:
+              if x.vowel == a+A and x.post_cons_str[-1] == A and not x.doubled and not x.suff:
                 x.hmz = A_md
               else:
                 pass
@@ -191,7 +191,7 @@ def hamzate(instr, suf):
             else:
               pass
         elif prev_cons.vowel_mark == o:
-          if x.suf or index == (len(cons_list_base)-1):
+          if x.suff or index == (len(cons_list_base)-1):
             pass
           else:
             if x.vowel_mark == i:
@@ -242,102 +242,99 @@ def hamzate(instr, suf):
         x.hmz = tatw+hmz_ab
 
   outstr = build_str(cons_list)
-  return outstr
-
-do_test = False
-if do_test:
-  word_list = ["ءِسْلَام", "ءَامَنَ", "ءَسْلَمَ", "ءُرِيدُ", "ءِسْلَام", "ءِيمَان", "ءُوخِذَ", "دُعَاءُ", "سُوءُ", "جِيءَ", "ضَوْءَ", "شَيْءَ", "بُطْءُ", "عِبْءُ", "شَطْءُ", "تَبَوُّءُ", "يُهَدِّء", "سَيِّء", "بَطُء", "يَهْدَء", "مُبْتَدَء", "هَيْءَة", "خَطِيءَة", "بَرِيءُونَ", "بَرِيءَانِ", "بَرِيءِينَ", "بَرِيءَيْنِ", "شَيْءُهُ", "شَيْءَهُ", "شَيْءِهِ", "شَيْءَانِ", "شَيْءَيْنِ", "مَجِيءُهُ", "مَجِيءَهُ", "مَجِيءِهِ", "سُوءِهِ", "ضَوْءِهِ", "سُوءَهُ", "سُوءَانِ", "تَوْءَم", "ضَوْءَهُ", "ضَوْءَانِ", "سُوءُهُ", "يَسُوءُونَ", "سَاءِل", "تَسَاءُل", "تَسَاءَلَ", "قِرَاءَات", "جُزْءَانِ", "عِبْءَانِ", "عِبْءَيْنِ", "بُطْءَهُ", "بُطْءُهُ", "بُطْءِهِ", "مَسْءُول", "تَرْءِيس", "مِرْءَاة", "ظَمْءَان", "مَسْءَلَة", "الْمَرْءَة", "بِءْسَ", "سُءْلَكَ", "كَءْس", "سُءِلَ", "يَءِسَ", "مُتَّكِءِينَ", "رَءِيس", "سُءَال", "رُءُوس", "لُءَيّ", "شَنَءَان", "سَءَلَ", "رَءَىٰ", "رَءَسَ", "يُرَءِسُ", "رُءِّسَ", "تَفَءُّل", "يُبَرِّءُونَ", "يُبَرَّءُونَ", "سَءَّال", "لَءَّال"]
-
-  for x in word_list:
-    print(hamzate(x))
-
-#import sys
-#print(hamzate(sys.argv[1]))
+  return pref_str + outstr
 
 def test():
   pairs = [
-    (("'aAmana", ""), "|mana"),
-    (("'asolama", ""), "A^asolama"),
-    (("'uriydu", ""), "A^uriydu"),
-    (("'isolaAm", ""), "A/isolaAm"),
-    (("'iymaAn", ""), "A/iymaAn"),
-    (("'uwxi*a", ""), "A^uwxi*a"),
-    (("hayo'ap", ""), "hayo'ap"),
-    (("xaTiy'ap", ""), "xaTiy'ap"),
-    (("bariy'", "uwn"), "bariy'uwn"),
-    (("bariy'", "aAni"), "bariy'aAni"),
-    (("$ayo'u", "hu"), "$ayo'uhu"),
-    (("$ayo'a", "hu"), "$ayo'ahu"),
-    (("$ayo'i", "hu"), "$ayo'ihu"),
-    (("majiy'u", "hu"), "majiy'uhu"),
-    (("majiy'a", "hu"), "majiy'ahu"),
-    (("majiy'i", "hu"), "majiy'ihu"),
-    (("Aistayo'aAs", ""), "Aistayo'aAs"),
-    (("Aistiy'aAr", ""), "Aistiy'aAr"),
-    (("Aistiy'aAlN", ""), "Aistiy'aAlN"),
-    (("suw'i", "hi"), "suwy^ihi"),
-    (("Daw'i", "hi"), "Dawy^ihi"),
-    (("suw'a", "hu"), "suw'ahu"),
-    (("suw'", "aAn"), "suw'aAn"),
-    (("tawo'am", ""), "tawo'am"),
-    (("Dawo'a", "hu"), "Dawo'ahu"),
-    (("Dawo'", "aAn"), "Dawo'aAn"),
-    (("suw'u", "hu"), "suw'uhu"),
-    (("yasuw'", "uwna"), "yasuw'uwna"),
-    (("nuw'aAn", ""), "nuw|n"),
-    (("saA'il", ""), "saAy^il"),
-    (("tasaA'ul", ""), "tasaAw^ul"),
-    (("tasaA'ala", ""), "tasaA'ala"),
-    (("qiraA'aAt", ""), "qiraA'aAt"),
-    (("juzo'", "aAn"), "juzo'aAn"),
-    (("Eibo'", "aAni"), "Eibo'aAni"),
-    (("Eibo'", "ayoni"), "Eibo'ayoni"),
-    (("buTo'a", "hu"), "buTo'ahu"),
-    (("buTo'", "uhu"), "buTo'uhu"),
-    (("buTo'", "ihi"), "buTo'ihi"),
-    (("maso'uwl", ""), "masow^uwl"),
-    (("taro'iys", ""), "taroy^iys"),
-    (("miro'aAp", ""), "miro|p"),
-    (("Zamo'aAnu", ""), "Zamo|nu"),
-    (("maso'alap", ""), "masoA^alap"),
-    (("Alomaro'ap", ""), "AlomaroA^ap"),
-    (("bi'osa", ""), "biy^osa"),
-    (("mut~aki'", "iyna"), "mut~akiy^iyna"),
-    (("ra'iys", ""), "ray^iys"),
-    (("su'aAl", ""), "suw^aAl"),
-    (("ru'uws", ""), "ruw^uws"),
-    (("lu'ay~", ""), "luw^ay~"),
-    (("$ana'aAn", ""), "$ana|n"),
-    (("sa'ala", ""), "saA^ala"),
-    (("ra'aY", ""), "raA^aY"),
-    (("ra'~asa", ""), "raA^~asa"),
-    (("ru'~isa", ""), "ruy^~isa"),
-    (("tafa'~ul", ""), "tafaw^~ul"),
-    (("yubar~i'", "uwn"), "yubar~iy^uwn"),
-    (("yubar~a'", "uwn"), "yubar~aw^uwn"),
-    (("sa'~aAl", ""), "sa'~aAl"),
-    (("la'~aAl", ""), "la'~aAl"),
-    (("duEaA'u", ""), "duEaA'u"),
-    (("suw'u", ""), "suw'u"),
-    (("jiy'a", ""), "jiy'a"),
-    (("Dawo'a", ""), "Dawo'a"),
-    (("$ayo'a", ""), "$ayo'a"),
-    (("Eibo'u", ""), "Eibo'u"),
-    (("$aTo'u", ""), "$aTo'u"),
-    (("tabaw~u'", ""), "tabaw~u'"),
-    (("yuhad~i'u", ""), "yuhad~iy^u"),
-    (("say~i'u", ""), "say~iy^u"),
-    (("baTu'a", ""), "baTuw^a"),
-    (("yahoda'u", ""), "yahodaA^u"),
-    (("mubotada'i", ""), "mubotadaA/i"),
+    (("'aAmana", "", ""), "|mana"),
+    (("'asolama", "", ""), "A^asolama"),
+    (("'uriydu", "", ""), "A^uriydu"),
+    (("'isolaAm", "", ""), "A/isolaAm"),
+    (("'iymaAn", "", ""), "A/iymaAn"),
+    (("'uwxi*a", "", ""), "A^uwxi*a"),
+    (("'aAmana", "", "la"), "la|mana"),
+    (("'asolama", "", "li"), "liA^asolama"),
+    (("'uriydu", "", "fa"), "faA^uriydu"),
+    (("'isolaAm", "", "bi"), "biA/isolaAm"),
+    (("'iymaAn", "", "bi"), "biA/iymaAn"),
+    (("'uwxi*a", "", "fa"), "faA^uwxi*a"),
+    (("hayo'ap", "", ""), "hayo'ap"),
+    (("xaTiy'ap", "", ""), "xaTiy'ap"),
+    (("bariy'", "uwn", ""), "bariy'uwn"),
+    (("bariy'", "aAni", ""), "bariy'aAni"),
+    (("$ayo'u", "hu", ""), "$ayo'uhu"),
+    (("$ayo'a", "hu", ""), "$ayo'ahu"),
+    (("$ayo'i", "hu", ""), "$ayo'ihu"),
+    (("majiy'u", "hu", ""), "majiy'uhu"),
+    (("majiy'a", "hu", ""), "majiy'ahu"),
+    (("majiy'i", "hu", ""), "majiy'ihu"),
+    (("Aistayo'aAs", "", ""), "Aistayo'aAs"),
+    (("Aistiy'aAr", "", ""), "Aistiy'aAr"),
+    (("Aistiy'aAlN", "", ""), "Aistiy'aAlN"),
+    (("suw'i", "hi", ""), "suwy^ihi"),
+    (("Daw'i", "hi", ""), "Dawy^ihi"),
+    (("suw'a", "hu", ""), "suw'ahu"),
+    (("suw'", "aAn", ""), "suw'aAn"),
+    (("tawo'am", "", ""), "tawo'am"),
+    (("Dawo'a", "hu", ""), "Dawo'ahu"),
+    (("Dawo'", "aAn", ""), "Dawo'aAn"),
+    (("suw'u", "hu", ""), "suw'uhu"),
+    (("yasuw'", "uwna", ""), "yasuw'uwna"),
+    (("nuw'aAn", "", ""), "nuw|n"),
+    (("saA'il", "", ""), "saAy^il"),
+    (("tasaA'ul", "", ""), "tasaAw^ul"),
+    (("tasaA'ala", "", ""), "tasaA'ala"),
+    (("qiraA'aAt", "", ""), "qiraA'aAt"),
+    (("juzo'", "aAn", ""), "juzo'aAn"),
+    (("Eibo'", "aAni", ""), "Eibo'aAni"),
+    (("Eibo'", "ayoni", ""), "Eibo'ayoni"),
+    (("buTo'a", "hu", ""), "buTo'ahu"),
+    (("buTo'", "uhu", ""), "buTo'uhu"),
+    (("buTo'", "ihi", ""), "buTo'ihi"),
+    (("maso'uwl", "", ""), "masow^uwl"),
+    (("taro'iys", "", ""), "taroy^iys"),
+    (("miro'aAp", "", ""), "miro|p"),
+    (("Zamo'aAnu", "", ""), "Zamo|nu"),
+    (("maso'alap", "", ""), "masoA^alap"),
+    (("Alomaro'ap", "", ""), "AlomaroA^ap"),
+    (("bi'osa", "", ""), "biy^osa"),
+    (("mut~aki'", "iyna", ""), "mut~akiy^iyna"),
+    (("ra'iys", "", ""), "ray^iys"),
+    (("su'aAl", "", ""), "suw^aAl"),
+    (("ru'uws", "", ""), "ruw^uws"),
+    (("lu'ay~", "", ""), "luw^ay~"),
+    (("$ana'aAn", "", ""), "$ana|n"),
+    (("sa'ala", "", ""), "saA^ala"),
+    (("ra'aY", "", ""), "raA^aY"),
+    (("ra'~asa", "", ""), "raA^~asa"),
+    (("ru'~isa", "", ""), "ruy^~isa"),
+    (("tafa'~ul", "", ""), "tafaw^~ul"),
+    (("yubar~i'", "uwn", ""), "yubar~iy^uwn"),
+    (("yubar~a'", "uwn", ""), "yubar~aw^uwn"),
+    (("sa'~aAl", "", ""), "sa'~aAl"),
+    (("la'~aAl", "", ""), "la'~aAl"),
+    (("duEaA'u", "", ""), "duEaA'u"),
+    (("suw'u", "", ""), "suw'u"),
+    (("jiy'a", "", ""), "jiy'a"),
+    (("Dawo'a", "", ""), "Dawo'a"),
+    (("$ayo'a", "", ""), "$ayo'a"),
+    (("Eibo'u", "", ""), "Eibo'u"),
+    (("$aTo'u", "", ""), "$aTo'u"),
+    (("tabaw~u'", "", ""), "tabaw~u'"),
+    (("yuhad~i'u", "", ""), "yuhad~iy^u"),
+    (("say~i'u", "", ""), "say~iy^u"),
+    (("baTu'a", "", ""), "baTuw^a"),
+    (("yahoda'u", "", ""), "yahodaA^u"),
+    (("mubotada'i", "", ""), "mubotadaA/i"),
     #(("", ""), ""),
   ]
 
   for pair in pairs:
     base_str = pair[0][0]
-    suf_str = pair[0][1]
+    suff_str = pair[0][1]
+    pref_str = pair[0][2]
     exp_str = pair[1]
-    out_str = hamzate(base_str, suf_str)
-    assert out_str == exp_str, "in=("+base_str+","+suf_str+"), exp="+exp_str+", got="+out_str
+    out_str = hamzate(base_str, suff_str, pref_str)
+    assert out_str == exp_str, "in=("+base_str+","+suff_str+"), exp="+exp_str+", got="+out_str
 
 test()
