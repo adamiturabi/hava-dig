d7d6ec50f6916fa0b5190b4b251470f50e582c5b
Adami Turabi <adamiturabi@protonmail.com>
2022-02-10 08:51:30 +0500

    adding passive voice. wip

--- src/conj.py
+++ src/conj.py
@@ -1,86 +1,129 @@
 from translit import *
 
-def conj_tri_form1(root, past_vowel, pres_vowel):
+def conj_tri_form1(root, past_vowel, pres_vowel, voice):
   r1 = root[0]
   r2 = root[1]
   r3 = root[2]
-  past_verb = r1 + "a" + r2 + past_vowel + r3 + "a"
-  pres_verb = "ya" + r1 + "o" + r2 + pres_vowel + r3 + "u"
+  if voice == "passive":
+    past_verb = r1 + u + r2 + i + r3 + a
+    pres_verb = "yu" + r1 + o + r2 + a + r3 + u
+  else:
+    past_verb = r1 + "a" + r2 + past_vowel + r3 + "a"
+    pres_verb = "ya" + r1 + "o" + r2 + pres_vowel + r3 + "u"
   if r2+r3 == "yy":
-    if past_vowel == "a":
-      past_verb = r1 + "a" + r2 + "~a"
-    if pres_vowel == "a":
-      pres_verb = "ya" + r1 + "o" + "yaY"
+    if voice == "passive":
+      past_verb = r1 + "u" + r2 + "~a"
     else:
-      pres_verb = "ya" + r1 + "o" + "yiy"
-  elif r2+r3 == "wy":
-    if past_vowel == "a":
-      past_verb = r1 + "a" + "waY"
-    if pres_vowel == "a":
-      pres_verb = "ya" + r1 + "o" + "waY"
-    else:
-      pres_verb = "ya" + r1 + "o" + "wiy"
-  elif r1 == "w" and r3 == "y":
-    if past_vowel == "a":
-      past_verb = r1 + "a" + r2 + past_vowel + "aY"
-    if pres_vowel == "a":
-      pres_verb = "ya" + r2 + "aY"
-    else:
-      pres_verb = "ya" + r2 + "iy"
-  elif r2+r3 == "y'":
-    if past_vowel != "i":
-      past_verb = r1 + "aA'a"
-    if pres_vowel == "a":
-      pres_verb = "ya" + r1 + "aA'u"
-    elif pres_vowel == "i":
-      pres_verb = "ya" + r1 + "iy'u"
-    else:
-      pres_verb = "ya" + r1 + "uwiu"
-  elif r3 == "y":
-    if past_vowel == "a":
-      past_verb = r1 + "a" + r2 + "a" + "Y"
-    if pres_vowel == "a":
-      pres_verb = "ya" + r1 + "o" + r2 + "aY"
-    else:
-      pres_verb = "ya" + r1 + "o" + r2 + "iy"
-  elif r3 == "w":
-    if past_vowel == "a":
-      if r2 == hmz:
-        past_verb = r1 + "a" + r2 + "a" + "Y"
+      if past_vowel == "a":
+        past_verb = r1 + "a" + r2 + "~a"
+      if pres_vowel == "a":
+        pres_verb = "ya" + r1 + "o" + "yaY"
       else:
-        past_verb = r1 + "a" + r2 + "a" + "A"
-    if pres_vowel == "u":
-      pres_verb = "ya" + r1 + "o" + r2 + "uw"
+        pres_verb = "ya" + r1 + "o" + "yiy"
+  elif r2+r3 == "wy":
+    if voice == "passive":
+      pass
+    else:
+      if past_vowel == "a":
+        past_verb = r1 + "a" + "waY"
+      if pres_vowel == "a":
+        pres_verb = "ya" + r1 + "o" + "waY"
+      else:
+        pres_verb = "ya" + r1 + "o" + "wiy"
+  elif r1 == "w" and r3 == "y":
+    if voice == "passive":
+      pass
+    else:
+      if past_vowel == "a":
+        past_verb = r1 + "a" + r2 + past_vowel + "aY"
+      if pres_vowel == "a":
+        pres_verb = "ya" + r2 + "aY"
+      else:
+        pres_verb = "ya" + r2 + "iy"
+  elif r2+r3 == "y'":
+    if voice == "passive":
+      pass
+    else:
+      if past_vowel != "i":
+        past_verb = r1 + "aA'a"
+      if pres_vowel == "a":
+        pres_verb = "ya" + r1 + "aA'u"
+      elif pres_vowel == "i":
+        pres_verb = "ya" + r1 + "iy'u"
+      else:
+        pres_verb = "ya" + r1 + "uwiu"
+  elif r3 == "y":
+    if voice == "passive":
+      pass
+    else:
+      if past_vowel == "a":
+        past_verb = r1 + "a" + r2 + "a" + "Y"
+      if pres_vowel == "a":
+        pres_verb = "ya" + r1 + "o" + r2 + "aY"
+      else:
+        pres_verb = "ya" + r1 + "o" + r2 + "iy"
+  elif r3 == "w":
+    if voice == "passive":
+      past_verb = r1 + u + r2 + i + "ya"
+    else:
+      if past_vowel == "a":
+        if r2 == hmz:
+          past_verb = r1 + "a" + r2 + "a" + "Y"
+        else:
+          past_verb = r1 + "a" + r2 + "a" + "A"
+      if pres_vowel == "u":
+        pres_verb = "ya" + r1 + "o" + r2 + "uw"
   elif r2 == "w":
-    if past_vowel != "i":
-      past_verb = r1 + "aA" + r3 + "a"
-    if pres_vowel == "a":
-      pres_verb = "ya" + r1 + "aA" + r3 + "u"
+    if voice == "passive":
+      pass
     else:
-      pres_verb = "ya" + r1 + "uw" + r3 + "u"
+      if past_vowel != "i":
+        past_verb = r1 + "aA" + r3 + "a"
+      if pres_vowel == "a":
+        pres_verb = "ya" + r1 + "aA" + r3 + "u"
+      else:
+        pres_verb = "ya" + r1 + "uw" + r3 + "u"
   elif r2 == "y":
-    if past_vowel != "i":
-      past_verb = r1 + "aA" + r3 + "a"
-    if pres_vowel == "a":
-      pres_verb = "ya" + r1 + "aA" + r3 + "u"
+    if voice == "passive":
+      pass
     else:
-      pres_verb = "ya" + r1 + "iy" + r3 + "u"
+      if past_vowel != "i":
+        past_verb = r1 + "aA" + r3 + "a"
+      if pres_vowel == "a":
+        pres_verb = "ya" + r1 + "aA" + r3 + "u"
+      else:
+        pres_verb = "ya" + r1 + "iy" + r3 + "u"
   elif r1 == "w":
-    pres_verb = "ya" + r2 + pres_vowel + r3 + "u"
+    if voice == "passive":
+      pass
+    else:
+      pres_verb = "ya" + r2 + pres_vowel + r3 + "u"
   elif r2 == r3:
-    past_verb = r1 + "a" + r2 + "~a"
-    pres_verb = "ya" + r1 + pres_vowel + r3 + "~u"
+    if voice == "passive":
+      past_verb = r1 + u + r2 + "~a"
+    else:
+      past_verb = r1 + "a" + r2 + "~a"
+      pres_verb = "ya" + r1 + pres_vowel + r3 + "~u"
   return (past_verb, pres_verb)
 
-def conj_tri_form2(root):
+def conj_tri_form2(root, voice):
   r1 = root[0]
   r2 = root[1]
   r3 = root[2]
-  past_verb = r1 + a + r2 + ss + a + r3 + a
-  pres_verb = "yu" + r1 + "a" + r2 + ss + i + r3 + u
+  if voice == "passive":
+    past_verb = r1 + u + r2 + ss + i + r3 + a
+    pres_verb = "yu" + r1 + a + r2 + ss + a + r3 + u
+  else:
+    past_verb = r1 + a + r2 + ss + a + r3 + a
+    pres_verb = "yu" + r1 + "a" + r2 + ss + i + r3 + u
+
   if r3 == y or r3 == w:
-    past_verb = r1 + a + r2 + ss + a + Y
-    pres_verb =  y + u + r1 + a + r2 + ss + i + y;
+    if voice == "passive":
+      past_verb = r1 + u + r2 + ss + i + y + a
+      pres_verb = "yu" + r1 + a + r2 + ss + a + Y
+    else:
+      past_verb = r1 + a + r2 + ss + a + Y
+      pres_verb =  y + u + r1 + a + r2 + ss + i + y;
   return (past_verb, pres_verb)
 
 def conj_tri_form3(root):
@@ -198,11 +241,11 @@ def conj_tri_form10(root):
     pres_verb = "yasota" + r1 + i + r2 + ss + u
   return (past_verb, pres_verb)
 
-def conj_tri(root, form, past_vowel, pres_vowel):
+def conj_tri(root, form, past_vowel, pres_vowel, voice):
   if form == 'I':
-    return conj_tri_form1(root, past_vowel, pres_vowel)
+    return conj_tri_form1(root, past_vowel, pres_vowel, voice)
   elif form =="II":
-    return conj_tri_form2(root)
+    return conj_tri_form2(root, voice)
   elif form =="III":
     return conj_tri_form3(root)
   elif form =="IV":
@@ -237,12 +280,12 @@ def conj_quad(root, form):
   else:
     assert False
     return ''
-def conj(root, form, vowel="a", tense="pret"):
+def conj(root, form, vowel="a", tense="pret", voice="active"):
   pret = form
   if len(root) == 2:
     root = root + root[1]
   if len(root) == 3:
-    verb_pair = conj_tri(root, form, vowel, vowel)
+    verb_pair = conj_tri(root, form, vowel, vowel, voice)
     #print(verb_pair[0])
     #print(bw_to_ar(verb_pair[1]))
     verb = verb_pair[0]
